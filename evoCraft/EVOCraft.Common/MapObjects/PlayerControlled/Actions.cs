9d105be537ed98f4586ed270074f551d05824f07
Balint Sedlak <balint.sedlak@evosoft.com>
2020-03-06 19:44:08 +0100

    Add EvoCraft from the 3rd semester of the evoCampus project.

---
+++ evoCraft/EVOCraft.Common/MapObjects/PlayerControlled/Actions.cs
@@ -0,0 +1,23 @@
+﻿namespace EvoCraft.Common
+{
+    public enum Actions
+    {
+        None,
+        Move,
+        Stop,
+        BuildMainHall,
+        BuildWall,
+        BuildBarracs,
+        BuildMedicalTent,
+        BuildTower,
+        BuildFarm,
+        TrainWorker,
+        TrainSoldier,
+        TrainDoctor,
+        TrainHero,
+        Cancel,
+        AutoHeal,
+        AutoAttack,
+        TrainGunMan
+    }
+}
